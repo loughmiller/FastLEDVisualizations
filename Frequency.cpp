b1633f6d463416ff18452a0a01c2a34dd1e6c1ee
Scott Loughmiller <scott@scalecomputing.com>
2017-07-02 16:01:02 -0700

    frequency working - hardcoded to 10 columns

--- Frequency.cpp
+++ Frequency.cpp
@@ -10,20 +10,41 @@ Frequency::Frequency (int columns, int rows, CRGB * leds, CRGB color)
 }
 
 void Frequency::display(unsigned long currentTime, int * levels) {
-  int currentFrame = this->frame % 100;
-  // Serial.println(currentTime, this->nextTime);
-
-  if (currentTime > this->nextTime) {
-    for(int i=0;i<7;i++)
-    {
-      Serial.print(levels[i]);
-      if(i<6)  Serial.print(",");
-      else Serial.println();
-      levels[i] = 0;
-    }
-
-    this->frame++;
-    this->nextTime = currentTime + this->interval;
+  // FREQ_0
+  for(int y=0; y<levels[0]/17; y++) {
+    this->leds[this->xy2Pos(0, 15 - y)] = this->color;
+    this->leds[this->xy2Pos(5, 15 - y)] = this->color;
   }
 
+  // FREQ_1
+  for(int y=0; y<levels[1]/17; y++) {
+    this->leds[this->xy2Pos(1, 15 - y)] = this->color;
+    this->leds[this->xy2Pos(6, 15 - y)] = this->color;
+  }
+
+  // FREQ_2
+  for(int y=0; y<levels[2]/17; y++) {
+    this->leds[this->xy2Pos(2, 15 - y)] = this->color;
+    this->leds[this->xy2Pos(7, 15 - y)] = this->color;
+  }
+
+  // FREQ_3
+  for(int y=0; y<levels[3]/17; y++) {
+    this->leds[this->xy2Pos(3, 15 - y)] = this->color;
+  }
+
+  // FREQ_4
+  for(int y=0; y<levels[4]/17; y++) {
+    this->leds[this->xy2Pos(8, 15 - y)] = this->color;
+  }
+
+  // FREQ_5
+  for(int y=0; y<levels[5]/17; y++) {
+    this->leds[this->xy2Pos(4, 15 - y)] = this->color;
+  }
+
+  // FREQ_6
+  for(int y=0; y<levels[6]/17; y++) {
+    this->leds[this->xy2Pos(9, 15 - y)] = this->color;
+  }
 }
