555ef4a160317af5630050597f028b2a0d5159c4
Scott Loughmiller <scott@scalecomputing.com>
2017-08-26 17:45:38 -0700

    adjusted to support FFT lib change

--- Spectrum.cpp
+++ Spectrum.cpp
@@ -17,11 +17,11 @@ Spectrum::Spectrum(uint16_t columns,
 void Spectrum::display(float* intensities) {
   for (uint8_t y=0; y<this->rows - this->rowOffset; y++) {
     float intensity = intensities[y+2];
-    if (intensity < 0.3) {
+    if (intensity < 0.85) {
       continue;
     }
 
-    intensity = (intensity - (0.3)) / 0.7;
+    intensity = (intensity - (0.85)) / 0.15;
     uint8_t hue = (this->travel * intensity) + this->hue;
 
     CRGB c = CHSV(hue, this->saturation, 255);
