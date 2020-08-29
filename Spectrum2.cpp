d12b54284ecc75564e44be74b709be1800025b17
Scott Loughmiller <scott@scalecomputing.com>
2020-08-29 11:01:08 -0700

    use 3 lines for over-peak notes

--- Spectrum2.cpp
+++ Spectrum2.cpp
@@ -12,7 +12,7 @@ Spectrum2::Spectrum2(uint16_t columns,
   this->rowOffset = rowOffset;
   this->invert = invert;
   this->length = length;
-  this->density = 0.15;
+  this->density = 0.06;
   this->threshold = 1000.0;
   this->peak = 2000.0;
   this->drift = 0;
@@ -20,6 +20,8 @@ Spectrum2::Spectrum2(uint16_t columns,
 }
 
 void Spectrum2::display(float* magnitudes) {
+  uint_fast16_t peakCount = 0;
+  bool overPeak = false;
 
   float sorted[this->length];
   memcpy(sorted, magnitudes, sizeof(magnitudes[0]) * this->length);
@@ -34,27 +36,55 @@ void Spectrum2::display(float* magnitudes) {
   float magnitudeSum = 0;
 
   for (uint8_t y=0; y<this->length; y++) {
+    overPeak = false;
     magnitudeSum += magnitudes[y];
 
     if (magnitudes[y] < this->threshold) {
       continue;
     }
 
-    magnitude = ((magnitudes[y] - threshold) / (peak - threshold));
+    if (magnitudes[y] > this->peak * 1.2) {
+      peakCount++;
+      overPeak = true;
+    }
+
+    magnitude = ((magnitudes[y] - this->threshold) / (this->peak - this->threshold));
     magnitude = min(magnitude, 1);
+    magnitude = max(magnitude, 0.15);
 
     CRGB c = CHSV(this->hue, this->saturation, magnitude*255);
+    CRGB c2 = CHSV(this->hue, this->saturation, magnitude*128);
     for (uint8_t x=0; x<this->columns; x++) {
       if (this->invert) {
         leds[this->xy2Pos(x, this->rowOffset - y)] = c;
+        if (overPeak) {
+          if (y > 0) {
+            leds[this->xy2Pos(x, (this->rowOffset - (y - 1)))] = c2;
+          }
+          if (y < this->length - 1) {
+            leds[this->xy2Pos(x, (this->rowOffset - (y + 1)))] = c2;
+          }
+        }
       } else {
         leds[this->xy2Pos(x, y + this->rowOffset)] = c;
+        if (overPeak) {
+          if (y > 0) {
+            leds[this->xy2Pos(x, (y + this->rowOffset) - 1)] = c2;
+          }
+          if (y < this->length - 1) {
+            leds[this->xy2Pos(x, y + this->rowOffset + 1)] = c2;
+          }
+        }
       }
+    }
+    if (overPeak) {
+      y++;
     }
   }
 
   this->hue += this->drift;
 
+  // Change hue to pink on big volume increases
   if (this->drift > 0 && magnitudeSum > this->totalMagnitudeMovingAverage * 1.75) {
     this->hue = 240;
   }
@@ -62,27 +92,28 @@ void Spectrum2::display(float* magnitudes) {
   this->totalMagnitudeMovingAverage = (this->totalMagnitudeMovingAverage * (0.9998)) + (magnitudeSum/5000.0);
 
 
-  // uint_fast32_t currentTime = millis();
+  uint_fast32_t currentTime = millis();
 
-  // // put things we want to log here
-  // if (currentTime > this->loggingTimestamp + 5000) {
-  //   this->loggingTimestamp = currentTime;
+  // put things we want to log here
+  if (currentTime > this->loggingTimestamp + 5000) {
+    this->loggingTimestamp = currentTime;
 
-  //   Serial.print(cutoffMagnitude);
-  //   Serial.print("\t");
-  //   Serial.print(peakMagnitude);
-  //   Serial.print("\t");
-  //   Serial.print(this->threshold);
-  //   Serial.print("\t");
-  //   Serial.print(this->peak);
-  //   Serial.print("\t");
-  //   Serial.print(this->hue);
-  //   Serial.print("\t");
-  //   Serial.print(magnitudeSum);
-  //   Serial.print("\t");
-  //   Serial.print(this->totalMagnitudeMovingAverage);
-  //   Serial.println("");
-  // }
+    Serial.print(peakCount);
+    // Serial.print(cutoffMagnitude);
+    // Serial.print("\t");
+    // Serial.print(peakMagnitude);
+    // Serial.print("\t");
+    // Serial.print(this->threshold);
+    // Serial.print("\t");
+    // Serial.print(this->peak);
+    // Serial.print("\t");
+    // Serial.print(this->hue);
+    // Serial.print("\t");
+    // Serial.print(magnitudeSum);
+    // Serial.print("\t");
+    // Serial.print(this->totalMagnitudeMovingAverage);
+    Serial.println("");
+  }
 }
 
 void Spectrum2::setDrift(uint8_t drift) {
